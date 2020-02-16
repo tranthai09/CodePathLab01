f65300c866ee8031b594e48975fe1f87cf5678f8
tranthai <tranthai@vt.edu>
2020-02-16 11:55:53 -0500

    added background colors card

--- FlashRev/app/src/main/java/com/example/flashrev/MainActivity.java
+++ FlashRev/app/src/main/java/com/example/flashrev/MainActivity.java
@@ -20,7 +20,37 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.red));
+        findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.flashcard_answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer1).setBackgroundColor(getResources().getColor(R.color.red));
+                findViewById(R.id.flashcard_correctAnswer).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+            }
+        });
+
+        findViewById(R.id.flashcard_answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer2).setBackgroundColor(getResources().getColor(R.color.red));
+                findViewById(R.id.flashcard_correctAnswer).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+            }
+        });
+
+        findViewById(R.id.flashcard_correctAnswer).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_correctAnswer).setBackgroundColor(getResources().getColor(R.color.lightGreen));
+            }
+        });
+
     }
 
 }
