5f4863765847b8d2913c8c96ea33ecbc77ca9ef2
tranthai <tranthai@vt.edu>
2020-02-15 22:54:43 -0500

    First Commit

---
+++ FlashRev/app/src/main/java/com/example/flashrev/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.flashrev;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
+            }
+        });
+
+        //findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.red));
+    }
+
+}
