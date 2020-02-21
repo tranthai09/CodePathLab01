634aa5c7b08341f4002726c355344277a5fe7502
tranthai <tranthai@vt.edu>
2020-02-21 01:52:44 -0500

    Toggling hide/show answer choices

--- FlashRev/app/src/main/java/com/example/flashrev/MainActivity.java
+++ FlashRev/app/src/main/java/com/example/flashrev/MainActivity.java
@@ -2,10 +2,13 @@ package com.example.flashrev;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isShowingAnswers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +53,40 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.flashcard_correctAnswer).setBackgroundColor(getResources().getColor(R.color.lightGreen));
             }
         });
+
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isShowingAnswers){
+                    ((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.open_eye);
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.flashcard_correctAnswer).setVisibility(View.INVISIBLE);
+                }
+                //isShowingAnswers = true;
+
+            }
+        });
+
+        findViewById(R.id.toggle_choices_invisibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (isShowingAnswers){
+                    findViewById(R.id.toggle_choices_visibility).setVisibility(View.INVISIBLE);
+                    ((ImageView) findViewById(R.id.toggle_choices_invisibility)).setImageResource(R.drawable.closed_eye);
+
+                    findViewById(R.id.flashcard_answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.flashcard_correctAnswer).setVisibility(View.VISIBLE);
+                }
+
+
+            }
+        });
+
+
+
+
 
     }
 
